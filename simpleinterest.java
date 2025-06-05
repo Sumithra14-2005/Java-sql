82d5a419468fce63e56a3efebad4723a5ad0b2a7
Sumithra14-2005 <sumithrasenthilkumar070@gmail.com>
2025-06-05 14:58:01 +0530

    Add files via upload

---
+++ simpleinterest.java
@@ -0,0 +1,18 @@
+package program;
+
+public class simpleinterest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int p=1000;
+int r=5;
+int t=2;
+
+System.out.println(p*r*t/100);
+
+	
+	
+	
+	}
+
+}
